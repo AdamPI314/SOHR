a16893ba085498649c9d29587ed4429591c8faea
AdamPI <shirong.bai@colorado.edu>
2017-07-14 02:24:48 -0600

    Update genetic toggle switch python files.

--- cplusplus_lib/tools/data_analysis/genetic_toggle_switch/concatenate_results.py
+++ cplusplus_lib/tools/data_analysis/genetic_toggle_switch/concatenate_results.py
@@ -1,71 +1,41 @@
-import os
-import sys
 import numpy as np
-import read_write_configuration as rwc
+import os
 
 
-def concatenate_time(file_dir, p_iter_N=3):
-    setting_d = rwc.read_configuration(os.path.join(file_dir, 'input', 'setting.json'))
-    time_iteration_number = setting_d['SOHR_init']['timeIterationNumber']
-    if time_iteration_number == 1:
-        time = np.loadtxt(os.path.join(file_dir, 'output', 'time_SOHR_M_' + str(p_iter_N) + '.csv'))
-        np.savetxt(os.path.join(file_dir, 'output', 'time_SOHR_M_all.csv'), time)
+def concatenate_time(file_dir, counter=0):
+    time = np.loadtxt(os.path.join(file_dir, 'output', 'time_ssa_number.csv'))
+    if counter == 0:
+        np.savetxt(os.path.join(file_dir, 'output', 'time_ssa_number_all.csv'), time)
     else:
-        time_o = np.loadtxt(os.path.join(file_dir, 'output', 'time_SOHR_M_all.csv'))
-        time_n = np.loadtxt(os.path.join(file_dir, 'output', 'time_SOHR_M_' + str(p_iter_N) + '.csv'))
-        time_n += time_o[-1]
-
-        with open(os.path.join(file_dir, 'output', 'time_SOHR_M_all.csv'), 'ab') as fp:
-            np.savetxt(fp, time_n[1::])
+        with open(os.path.join(file_dir, 'output', 'time_ssa_number_all.csv'), 'ab') as fp:
+            np.savetxt(fp, time)
 
 
-def concatenate_concentration(file_dir, p_iter_N=3):
-    setting_d = rwc.read_configuration(os.path.join(file_dir, 'input', 'setting.json'))
-    time_iteration_number = setting_d['SOHR_init']['timeIterationNumber']
-    for p_iter_i in range(1, p_iter_N+1):
-        concentration = np.loadtxt(
-                os.path.join(file_dir, 'output', 'concentration_SOHR_M_' + str(p_iter_i) + '.csv'),
-                delimiter=',')
-        if time_iteration_number == 1:
-            np.savetxt(os.path.join(file_dir, 'output', 'concentration_SOHR_M_all_'+ str(p_iter_i)+ '.csv'), concentration,
-                    delimiter=',')
-            # last iteration, save one more copy as all
-            if p_iter_i == p_iter_N: 
-                np.savetxt(os.path.join(file_dir, 'output', 'concentration_SOHR_M_all.csv'), concentration,
-                        delimiter=',')
-        else:
-            with open(os.path.join(file_dir, 'output', 'concentration_SOHR_M_all_'+ str(p_iter_i)+ '.csv'), 'ab') as fp:
-                np.savetxt(fp, concentration[1::, :], delimiter=',')
-            # last iteration, save one more copy as all
-            if p_iter_i == p_iter_N: 
-                with open(os.path.join(file_dir, 'output', 'concentration_SOHR_M_all.csv'), 'ab') as fp:
-                    np.savetxt(fp, concentration[1::, :], delimiter=',')
-
-
-
-def concatenate_temperature(file_dir, p_iter_N=3):
-    setting_d = rwc.read_configuration(os.path.join(file_dir, 'input', 'setting.json'))
-    time_iteration_number = setting_d['SOHR_init']['timeIterationNumber']
-    if time_iteration_number == 1:
-        temperature = np.loadtxt(os.path.join(file_dir, 'output', 'temperature_SOHR_M_' + str(p_iter_N) + '.csv'))
-        np.savetxt(os.path.join(file_dir, 'output', 'temperature_SOHR_M_all.csv'), temperature)
+def concatenate_concentration(file_dir, counter=0):
+    concentration = np.loadtxt(
+        os.path.join(file_dir, 'output', 'concentration_ssa_number.csv'), delimiter=',')
+    if counter == 0:
+        np.savetxt(os.path.join(file_dir, 'output', 'concentration_ssa_number_all.csv'), concentration,
+                   delimiter=',')
     else:
-        temperature = np.loadtxt(
-                os.path.join(file_dir, 'output', 'temperature_SOHR_M_' + str(p_iter_N) + '.csv'))
-        with open(os.path.join(file_dir, 'output', 'temperature_SOHR_M_all.csv'), 'ab') as fp:
-            np.savetxt(fp, temperature[1::])
+        with open(os.path.join(file_dir, 'output', 'concentration_ssa_number_all.csv'), 'ab') as fp:
+            np.savetxt(fp, concentration, delimiter=',')
 
 
-def concatenate_pressure(file_dir, p_iter_N=3):
-    setting_d = rwc.read_configuration(os.path.join(file_dir, 'input', 'setting.json'))
-    time_iteration_number = setting_d['SOHR_init']['timeIterationNumber']
-    if time_iteration_number == 1:
-        pressure = np.loadtxt(os.path.join(file_dir, 'output', 'pressure_SOHR_M_' + str(p_iter_N) + '.csv'),
-                delimiter=',')
-        np.savetxt(os.path.join(file_dir, 'output', 'pressure_SOHR_M_all.csv'), pressure, delimiter=',')
+def concatenate_temperature(file_dir, counter=3):
+    temperature = np.loadtxt(os.path.join(file_dir, 'output', 'temperature_ssa_number.csv'))
+    if counter == 0:
+        np.savetxt(os.path.join(file_dir, 'output', 'temperature_ssa_number_all.csv'), temperature)
     else:
-        pressure = np.loadtxt(
-                os.path.join(file_dir, 'output', 'pressure_SOHR_M_' + str(p_iter_N) + '.csv'),
-                delimiter=',')
-        with open(os.path.join(file_dir, 'output', 'pressure_SOHR_M_all.csv'), 'ab') as fp:
-            np.savetxt(fp, pressure[1::, :], delimiter=',')
+        with open(os.path.join(file_dir, 'output', 'temperature_ssa_number_all.csv'), 'ab') as fp:
+            np.savetxt(fp, temperature)
+
+
+def concatenate_pressure(file_dir, counter=3):
+    pressure = np.loadtxt(os.path.join(file_dir, 'output', 'pressure_ssa_number.csv'))
+    if counter == 0:
+        np.savetxt(os.path.join(file_dir, 'output', 'pressure_ssa_number_all.csv'), pressure)
+    else:
+        with open(os.path.join(file_dir, 'output', 'pressure_ssa_number_all.csv'), 'ab') as fp:
+            np.savetxt(fp, pressure)
+
--- cplusplus_lib/tools/data_analysis/genetic_toggle_switch/main_driver.py
+++ cplusplus_lib/tools/data_analysis/genetic_toggle_switch/main_driver.py
@@ -1,9 +1,7 @@
 import os
 import sys
-import update_settings as us
 import job_drivers
 import concatenate_results as cr
-import read_write_configuration as rwc
 import time
 
 if __name__ == '__main__':
@@ -11,39 +9,27 @@ if __name__ == '__main__':
 
     file_dir = os.path.abspath(os.path.join(os.path.realpath(sys.argv[0]), os.pardir, os.pardir, os.pardir, os.pardir))
 
-    time_iter_counter = 0 
-    time_iter_N = 3 
-    dt = 10.0 
+    counter = 0 
+    totalN = 2 
 
-    while time_iter_counter < time_iter_N:
-        # update settings
-        us.update_settings(file_dir, time_iter_counter, dt=dt)
+    while counter < totalN:
+        # # run jobs
+        # try:
+        #     job_drivers.path_make_run(file_dir)
+        # except RuntimeError:
+        #     print("RuntimeError")
+        #     exit()
 
-        # run jobs
-        job_drivers.delete_SOHR_temp_files(file_dir)
-        try:
-            job_drivers.path_ode_run(file_dir)
-        except RuntimeError:
-            print("RuntimeError")
-            exit()
-
-        p_iter_N = rwc.read_iteration_Number(os.path.join(file_dir, "input", "setting.json"))
         # organize files. concatenate into one file
-        cr.concatenate_time(file_dir, p_iter_N=p_iter_N)
-        cr.concatenate_concentration(file_dir, p_iter_N=p_iter_N)
-        cr.concatenate_temperature(file_dir, p_iter_N=p_iter_N)
-        cr.concatenate_pressure(file_dir, p_iter_N=p_iter_N)
+        cr.concatenate_time(file_dir, counter)
+        cr.concatenate_concentration(file_dir, counter)
+        cr.concatenate_temperature(file_dir, counter)
+        cr.concatenate_pressure(file_dir, counter)
 
-        time_iter_counter += 1
-
-    # run dlosde
-    job_drivers.run_dlsode(file_dir)
-
-    # make figures
-    job_drivers.make_figures(file_dir)
+        counter += 1
 
     # send email
-    job_drivers.send_email(file_dir)
+    # job_drivers.send_email(file_dir)
 
     time_e = time.time()
     print("running time:\t" + str("{:.2f}".format((time_e - time_i)/3600.0)) + " hours\n")
