b15d669722f405ec494338ee7f1006888e901b4c
AdamPI <shirong.bai@colorado.edu>
2017-07-13 18:49:15 -0600

    Update plot ssa.

--- cplusplus_lib/tools/data_analysis/genetic_toggle_switch/plot_ssa_delta.py
+++ cplusplus_lib/tools/data_analysis/genetic_toggle_switch/plot_ssa_delta.py
@@ -59,9 +59,9 @@ if __name__ == '__main__':
 
     N1 = -10
     counter = 0
-    ax.plot(time[N1:], data[N1:], '-', color=colors[counter % len(colors)], linewidth=1.0, label='$N_A-N_B$')
+    ax.plot(time[N1:]-time[N1], data[N1:], '-', color=colors[counter % len(colors)], linewidth=1.0, label='$N_A-N_B$')
 
-    ax.set_xbound(time[N1], time[-1])
+    ax.set_xbound(0, time[-1]-time[N1])
     ax.grid('on')
 
     ax.legend(loc=0, prop={'size': 10.0})
