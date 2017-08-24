9345ef33607f804e2e8ee9782fe909586063bc07
AdamPI <shirong.bai@colorado.edu>
2017-08-23 21:29:36 -0600

    macros.

--- fortran_lib/fortran_lib/use_chemkin.f
+++ fortran_lib/fortran_lib/use_chemkin.f
@@ -49,329 +49,7 @@
       End
 
 
-      Subroutine cppdlsodeV(ti,tout,neq,xgst)
-      ! at constant volume
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'xgst' is array of initial values, of length neq. The last value in this array is temperature. The others are mass fraction.
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11r
-
-      integer itol,mf,itask,istate,iopt,lrw,liw
-      double precision ti,tout,dt,atol,rtol,rjac
-      !parameter (lrw=500,liw=500)
-      !Have to make sure lrw, liw big enough, or it will have error like:
-      !*** glibc detected *** ./a.out: double free or corruption (top):
-      !*0x08901d70 ***
-      !======= Backtrace: =========
-      !/lib/libc.so.6(+0x6c501)[0x17c501]
-      !/lib/libc.so.6(+0x6dd70)[0x17dd70]
-      !/lib/libc.so.6(cfree+0x6d)[0x180e5d]
-      !/lib/libc.so.6(fclose+0x14a)[0x16c81a]
-      !./a.out[0x8048998]
-      !/lib/libpthread.so.0(+0x5cc9)[0xc1fcc9]
-      !/lib/libc.so.6(clone+0x5e)[0x1e069e]
-      !======= Memory map: ========
-      !parameter (lrw=500000,liw=500000)
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision xgst(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol
-
-      call dlsode (Fcn11r,neq,xgst,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,rjac,mf)
-
-      Return
-      End
-      
-      Subroutine cppdlsodeVT(ti,tout,neq,xgst)
-      ! at constant volume and constant temperature
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'xgst' is array of initial values, of length neq. The last value in this array is temperature. The others are mass fraction.
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rVT
-
-      integer itol,mf,itask,istate,iopt,lrw,liw
-      double precision ti,tout,dt,atol,rtol,rjac
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision xgst(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol
-
-      call dlsode (Fcn11rVT,neq,xgst,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,rjac,mf)
-
-      Return
-      End
-      
-      Subroutine cppdlsodeP(ti,tout,neq,xgst)
-      ! at constant pressure
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'xgst' is array of initial values, of length neq. The last value in this array is temperature. The others are mass fraction.
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rP
-
-      integer itol,mf,itask,istate,iopt,lrw,liw
-      double precision ti,tout,dt,atol,rtol,rjac
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision xgst(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol
-
-      call dlsode (Fcn11rP,neq,xgst,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,rjac,mf)
-
-      Return
-      End
-
-      Subroutine cppdlsodePT(ti,tout,neq,xgst)
-      ! at constant pressure and constant temperature
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'xgst' is array of initial values, of length neq. The last value in this array is temperature. The others are mass fraction.
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rPT
-
-      integer itol,mf,itask,istate,iopt,lrw,liw
-      double precision ti,tout,dt,atol,rtol,rjac
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision xgst(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol
-
-      call dlsode (Fcn11rPT,neq,xgst,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,rjac,mf)
-
-      Return
-      End
-
-      Subroutine cppdlsodeST(ti,tout,neq,ct)
-      ! surface reaction
-      ! at constant temperature
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'ct' is array of initial values, of length neq. The last value in this array is temperature. The others are molar concentration
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rST
-
-      integer itol,mf,itask,istate,iopt,lrw,liw
-      double precision ti,tout,dt,atol,rtol,rjac
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision ct(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol
-
-      call dlsode (Fcn11rST,neq,ct,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,rjac,mf)
-
-      Return
-      End
-
-      Subroutine cppdlsodaV(ti,tout,neq,xgst)
-      ! at constant volume
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'xgst' is array of initial values, of length neq. The last value in this array is temperature. The others are mass fraction.
-      ! lsoda, mf should be jt
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11r
-
-      integer itol,mf,itask,istate,iopt,lrw,liw,jt
-      double precision ti,tout,dt,atol,rtol,jdum
-
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision xgst(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol,jt
-
-      call dlsoda (Fcn11r,neq,xgst,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,jdum,jt)
-
-      Return
-      End
-
-      Subroutine cppdlsodaVT(ti,tout,neq,xgst)
-      ! at constant volume and temperature
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'xgst' is array of initial values, of length neq. The last value in this array is temperature. The others are mass fraction.
-      ! lsoda, mf should be jt
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rVT
-
-      integer itol,mf,itask,istate,iopt,lrw,liw,jt
-      double precision ti,tout,dt,atol,rtol,jdum
-
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision xgst(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol,jt
-
-      call dlsoda (Fcn11rVT,neq,xgst,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,jdum,jt)
-
-      Return
-      End      
-      
-      Subroutine cppdlsodaP(ti,tout,neq,xgst)
-      ! at constant pressure
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'xgst' is array of initial values, of length neq. The last value in this array is temperature. The others are mass fraction.
-      ! lsoda, mf should be jt
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rP
-
-      integer itol,mf,itask,istate,iopt,lrw,liw,jt
-      double precision ti,tout,dt,atol,rtol,jdum
-
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision xgst(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol,jt
-
-      call dlsoda (Fcn11rP,neq,xgst,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,jdum,jt)
-
-      Return
-      End
-
-      Subroutine cppdlsodaPT(ti,tout,neq,xgst)
-      ! at constant pressure and constant temperature
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'xgst' is array of initial values, of length neq. The last value in this array is temperature. The others are mass fraction.
-      ! lsoda, mf should be jt
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rPT
-
-      integer itol,mf,itask,istate,iopt,lrw,liw,jt
-      double precision ti,tout,dt,atol,rtol,jdum
-
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision xgst(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol,jt
-
-      call dlsoda (Fcn11rPT,neq,xgst,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,jdum,jt)
-
-      Return
-      End
-
-      Subroutine cppdlsodaST(ti,tout,neq,ct)
-      ! surface reaction
-      ! at constant temperature
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'ct' is array of initial values, of length neq. The last value in this array is temperature. The others are molar concentration
-      ! lsoda, mf should be jt
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rST
-
-      integer itol,mf,itask,istate,iopt,lrw,liw,jt
-      double precision ti,tout,dt,atol,rtol,jdum
-
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision ct(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol,jt
-
-      call dlsoda (Fcn11rST,neq,ct,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,jdum,jt)
-
-      Return
-      End
- 
-      Subroutine cppdlsodaSTCC1(ti,tout,neq,ct)
-	  ! hold the concentration of the first species to be constant
-      ! surface reaction
-      ! at constant temperature
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'ct' is array of initial values, of length neq. The last value in this array is temperature. The others are molar concentration
-      ! lsoda, mf should be jt
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rSTCC1
-
-      integer itol,mf,itask,istate,iopt,lrw,liw,jt
-      double precision ti,tout,dt,atol,rtol,jdum
-
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision ct(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol,jt
-
-      call dlsoda (Fcn11rSTCC1,neq,ct,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,jdum,jt)
-
-      Return
-      End 
-
-      Subroutine cppdlsodaSTCC2(ti,tout,neq,ct)
-	  ! hold the concentration of the first species to be constant
-      ! surface reaction
-      ! at constant temperature
-      ! 'ti' is the initial value of the independent variable.
-      ! 'tout' is the first point where output is desired (.ne. ti).
-      ! 'neq' is number of first order ode-s.
-      ! 'ct' is array of initial values, of length neq. The last value in this array is temperature. The others are molar concentration
-      ! lsoda, mf should be jt
-      implicit none
-      include "chemkin/ckvariables.f"
-      external Fcn11rSTCC2
-
-      integer itol,mf,itask,istate,iopt,lrw,liw,jt
-      double precision ti,tout,dt,atol,rtol,jdum
-
-      parameter (lrw=50000,liw=50000)
-      double precision rwork(lrw),iwork(liw)
-      integer neq
-      double precision ct(neq)
-      common /lsodestore/ atol,rtol,mf,itask,istate,iopt,itol,jt
-
-      call dlsoda (Fcn11rSTCC2,neq,ct,ti,tout,itol,rtol,atol,
-     >itask,istate,iopt,rwork,lrw,iwork,liw,jdum,jt)
-
-      Return
-      End 	  
-	  
-      Subroutine Fcn11r(neq,t,xgst,derstr)
+      Subroutine ChemkinUpdate(neq,t,xgst,derstr)
       ! at constant volume
       ! 'neq' is number of first order ode-s.
       ! 't' is a dummy variable here.
@@ -410,7 +88,7 @@
       Return
       End
 
-      Subroutine Fcn11rVT(neq,t,xgst,derstr)
+      Subroutine ChemkinUpdateVT(neq,t,xgst,derstr)
       ! at constant volume
       ! at constant temperature
       ! 'neq' is number of first order ode-s.
@@ -440,9 +118,9 @@
       derstr(neq)=0.0
 
       Return
-      End      
-      
-      Subroutine Fcn11rP(neq,t,xgst,derstr)
+      End
+
+      Subroutine ChemkinUpdateP(neq,t,xgst,derstr)
       ! at constant pressure
       ! 'neq' is number of first order ode-s.
       ! 't' is a dummy variable here.
@@ -491,7 +169,7 @@
       Return
       End
 
-      Subroutine Fcn11rPT(neq,t,xgst,derstr)
+      Subroutine ChemkinUpdatePT(neq,t,xgst,derstr)
       ! at constant pressure and constant temperature
       ! 'neq' is number of first order ode-s.
       ! 't' is a dummy variable here.
@@ -528,7 +206,7 @@
       Return
       End
 
-      Subroutine Fcn11rST(neq,t,ct,derstr)
+      Subroutine ChemkinUpdateST(neq,t,ct,derstr)
       ! surface reaction
       ! at constant temperature
       ! 'neq' is number of first order ode-s.
@@ -558,10 +236,10 @@
       derstr(neq)=0.0
 
       Return
-      End	  
+      End
 
-      Subroutine Fcn11rSTCC1(neq,t,ct,derstr)
-	  ! hold the concentration of the first species to be constant
+      Subroutine ChemkinUpdateSTCC1(neq,t,ct,derstr)
+      ! hold the concentration of the first species to be constant
       ! surface reaction
       ! at constant temperature
       ! 'neq' is number of first order ode-s.
@@ -589,14 +267,14 @@
 
       ! temperature is constant
       derstr(neq)=0.0
-	  ! the concentration of the first species to be constant
-	  derstr(1)=0.0
+      ! the concentration of the first species to be constant
+      derstr(1)=0.0
 
       Return
-      End		  
+      End
 
-      Subroutine Fcn11rSTCC2(neq,t,ct,derstr)
-	  ! hold the concentration of the first two species to be constant
+      Subroutine ChemkinUpdateSTCC2(neq,t,ct,derstr)
+      ! hold the concentration of the first two species to be constant
       ! surface reaction
       ! at constant temperature
       ! 'neq' is number of first order ode-s.
@@ -624,13 +302,13 @@
 
       ! temperature is constant
       derstr(neq)=0.0
-	  ! the concentration of the first two species to be constant
-	  derstr(1)=0.0
-	  derstr(2)=0.0
+      ! the concentration of the first two species to be constant
+      derstr(1)=0.0
+      derstr(2)=0.0
 
       Return
-      End	  
-	  
+      End
+
       Subroutine calculateTdotV(y, t, tdot)
       ! constant volume
       ! calculate temperature derivative at a initial temperature and initial mass fraction
@@ -653,7 +331,7 @@
 
       call ckums(t,ickwrk,rckwrk,h)
       call ckcvbs(t,y,ickwrk,rckwrk,cvbs)
-      
+
       sumh=0.d0
       do i=1,nkk
           sumh=sumh+wt(i)*wdot(i)*h(i)
