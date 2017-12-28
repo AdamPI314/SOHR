a72e9f43d0f8da8203d24ecdb2df017abc8b2510
AdamPI314 <shirong.bai@colorado.edu>
2017-12-28 13:19:34 -0700

    minor

--- input/local_settings.py
+++ input/local_settings.py
@@ -63,80 +63,93 @@ def get_local_settings():
     return setting
 
 
-def get_fast_rxn_trapped_spe():
+def get_fast_rxn_trapped_spe(atom_followed="C"):
     """
     return fast reaction and trapped species
     better make them in the same order
     """
     fast_transitions = [
+        # 1068    549     O2+npropyl=npropyloo
+        # reactants       9       O2      60      npropyl products        78      npropyloo
+        # 1069    -549    O2+npropyl=npropyloo
         {
-            # 1068    549     O2+npropyl=npropyloo
-            # reactants       9       O2      60      npropyl products        78      npropyloo
-            # 1069    -549    O2+npropyl=npropyloo
+            "formula1": "O2+npropyl=>npropyloo",
+            "formula2": "npropyloo=>O2+npropyl",
             "rxn": [1068, 1069],
-            "time_scale1": -10,
-            "time_scale2": -10,
             "spe": {
-                "C": [60, 78],
-                "O": [9, 78]
+                "H": [60, 78],
+                "O": [9, 78],
+                "C": [60, 78]
             }
         },
         # 1096    565     O2+ipropyl=ipropyloo
         # reactants       9       O2      61      ipropyl products        80      ipropyloo
         # 1097    -565    O2+ipropyl=ipropyloo
         {
+            "formula1": "O2+ipropyl=>ipropyloo",
+            "formula2": "ipropyloo=>O2+ipropyl",
             "rxn": [1096, 1097],
-            "spe": [61, 80]
-        },
-        # 132     69      CH3+O2(+M)=CH3OO(+M)
-        # reactants       25      CH3     9       O2      products        27      CH3OO
-        # 133     -69     CH3+O2(+M)=CH3OO(+M)
-        {
-            "rxn": [132, 133],
-            "spe": [25, 27]
+            "spe": {
+                "H": [61, 80],
+                "O": [9, 80],
+                "C": [61, 80]
+            }
         },
         # 1116    575     O2+QOOH_1=well_1
         # reactants       9       O2      87      QOOH_1  products        90      well_1
         # 1117    -575    O2+QOOH_1=well_1
         {
+            "formula1": "O2+QOOH_1=>well_1",
+            "formula2": "well_1=>O2+QOOH_1",
             "rxn": [1116, 1117],
-            "spe": [87, 90]
-        },
-        # 348     180     C2H5+O2=CH3CH2OO
-        # reactants       39      C2H5    9       O2      products        50      CH3CH2OO
-        # 349     -180    C2H5+O2=CH3CH2OO
-        {
-            "rxn": [348, 349],
-            "spe": [39, 50]
-        },
-        # 1080    556     npropyloo=QOOH_1        557     npropyloo=QOOH_1
-        # reactants       78      npropyloo       products        87      QOOH_1
-        # 1081    -556    npropyloo=QOOH_1        -557    npropyloo=QOOH_1
-        {
-            "rxn": [1080, 1081],
-            "spe": [78, 87]
-        },
-        # 586     300     O2C2H4OH=CH2CH2OH+O2
-        # reactants       85      O2C2H4OH        products        54      CH2CH2OH        9       O2
-        # 587     -300    O2C2H4OH=CH2CH2OH+O2
-        {
-            "rxn": [586, 587],
-            "spe": [85, 54]
-        },
-        # 1042    536     allyloxy=vinoxylmethyl
-        # reactants       72      allyloxy        products        108     vinoxylmethyl
-        # 1043    -536    allyloxy=vinoxylmethyl
-        {
-            "rxn": [1042, 1043],
-            "spe": [72, 108]
-        },
-        # 434     224     acetyl+O2=acetylperoxy
-        # reactants       9       O2      45      acetyl  products        47      acetylperoxy
-        # 435     -224    acetyl+O2=acetylperoxy
-        {
-            "rxn": [434, 435],
-            "spe": [45, 47]
+            "spe": {
+                "H": [87, 90],
+                "O": [9, 90],
+                "C": [87, 90]
+            }
         }
+        # # 132     69      CH3+O2(+M)=CH3OO(+M)
+        # # reactants       25      CH3     9       O2      products        27      CH3OO
+        # # 133     -69     CH3+O2(+M)=CH3OO(+M)
+        # {
+        #     "rxn": [132, 133],
+        #     "spe": [25, 27]
+        # },
+        # # 348     180     C2H5+O2=CH3CH2OO
+        # # reactants       39      C2H5    9       O2      products        50      CH3CH2OO
+        # # 349     -180    C2H5+O2=CH3CH2OO
+        # {
+        #     "rxn": [348, 349],
+        #     "spe": [39, 50]
+        # },
+        # # 1080    556     npropyloo=QOOH_1        557     npropyloo=QOOH_1
+        # # reactants       78      npropyloo       products        87      QOOH_1
+        # # 1081    -556    npropyloo=QOOH_1        -557    npropyloo=QOOH_1
+        # {
+        #     "rxn": [1080, 1081],
+        #     "spe": [78, 87]
+        # },
+        # # 586     300     O2C2H4OH=CH2CH2OH+O2
+        # # reactants       85      O2C2H4OH        products        54      CH2CH2OH        9       O2
+        # # 587     -300    O2C2H4OH=CH2CH2OH+O2
+        # {
+        #     "rxn": [586, 587],
+        #     "spe": [85, 54]
+        # },
+        # # 1042    536     allyloxy=vinoxylmethyl
+        # # reactants       72      allyloxy        products        108     vinoxylmethyl
+        # # 1043    -536    allyloxy=vinoxylmethyl
+        # {
+        #     "rxn": [1042, 1043],
+        #     "spe": [72, 108]
+        # },
+        # # 434     224     acetyl+O2=acetylperoxy
+        # # reactants       9       O2      45      acetyl  products        47      acetylperoxy
+        # # 435     -224    acetyl+O2=acetylperoxy
+        # {
+        #     "rxn": [434, 435],
+        #     "spe": [45, 47]
+        # }
 
     ]
 
@@ -147,7 +160,7 @@ def get_fast_rxn_trapped_spe():
         fast_reaction_list.append(
             tuple([str(r_s['rxn'][0]), int(r_s['rxn'][1])]))
         trapped_species_list.append(
-            tuple([str(r_s['spe'][0]), int(r_s['spe'][1])]))
+            tuple([str(r_s['spe'][atom_followed][0]), int(r_s['spe'][atom_followed][1])]))
 
     fast_reaction = OrderedDict(fast_reaction_list)
     trapped_species = OrderedDict(trapped_species_list)
